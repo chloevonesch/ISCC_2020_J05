8729682728c8a7f29957d004708cfeeed774a0b5
chloevonesch <chloe.vonesch@iseg.fr>
2020-06-29 14:22:53 +0200

    J05

---
+++ EX_02/footer.php
@@ -0,0 +1,8 @@
+<<<<<<< HEAD
+<footer>
+            <img src="assetsd04/logo_epitech.png" alt="logo epitech">
+=======
+<footer>
+            <img src="assetsd04/logo_epitech.png" alt="logo epitech">
+>>>>>>> 32a36bd3e11f1262c8aaaa1fef4315232de5fd10
+        </footer>
---
+++ EX_02/header.php
@@ -0,0 +1,14 @@
+<<<<<<< HEAD
+<header>
+        <!-- En-tête de la page -->
+        <h1>Summer Code Camp</h1>
+        <?php include("navigation.php"); ?>
+
+=======
+<header>
+        <!-- En-tête de la page -->
+        <h1>Summer Code Camp</h1>
+        <?php include("navigation.php"); ?>
+
+>>>>>>> 32a36bd3e11f1262c8aaaa1fef4315232de5fd10
+    </header>
